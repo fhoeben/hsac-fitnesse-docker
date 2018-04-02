f858babf7fdab08e44a7ee0c5d81672c19b8d724
Fried Hoeben <github@hsac.nl>
2018-04-02 16:11:29 +0200

    Also push base images

--- tagAndPush.sh
+++ tagAndPush.sh
@@ -2,13 +2,24 @@
 
 VERSION=$1
 
+echo "Pushing base-latest"
+docker push hsac/fitnesse-fixtures-test-jre8:base-latest
+
+docker tag hsac/fitnesse-fixtures-test-jre8:base-latest hsac/fitnesse-fixtures-test-jre8:base-${VERSION}
+echo "Pushing base-${VERSION}"
+docker push hsac/fitnesse-fixtures-test-jre8:base-${VERSION}
+
+echo "Pushing latest"
 docker push hsac/fitnesse-fixtures-test-jre8:latest
 
 docker tag hsac/fitnesse-fixtures-test-jre8:latest hsac/fitnesse-fixtures-test-jre8:${VERSION}
+echo "Pushing ${VERSION}"
 docker push hsac/fitnesse-fixtures-test-jre8:${VERSION}
 
+echo "Pushing latest"
 docker push hsac/fitnesse-fixtures-test-jre8-chrome:latest
 
 docker tag hsac/fitnesse-fixtures-test-jre8-chrome:latest hsac/fitnesse-fixtures-test-jre8-chrome:${VERSION}
+echo "Pushing ${VERSION}"
 docker push hsac/fitnesse-fixtures-test-jre8-chrome:${VERSION}
 

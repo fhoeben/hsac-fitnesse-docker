94b07d6f2b9bee9165043af37cfedffb8aaf8cca
Fried Hoeben <github@hsac.nl>
2018-08-12 18:20:16 +0200

    Latest selenium

--- buildChrome.sh
+++ buildChrome.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-SELENIUM_VERSION=${1:-3.12}
+SELENIUM_VERSION=${1:-3.14}
 
 BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 IMAGE=hsac/fitnesse-fixtures-test-jre8-chrome:latest

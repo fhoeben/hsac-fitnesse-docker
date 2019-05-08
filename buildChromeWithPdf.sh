8030069cdaac9f58eb039d30b9b07dc5e1512af6
Fried Hoeben <github@hsac.nl>
2019-05-08 18:50:13 +0200

    Different suite name for pdf image test runs

--- buildChromeWithPdf.sh
+++ buildChromeWithPdf.sh
@@ -8,9 +8,9 @@ IMAGE=hsac/fitnesse-fixtures-test-jre8-chrome-with-pdf:latest${VERSION_SUFFIX}
 docker build --squash -t ${IMAGE} chrome-with-pdf \
     && docker run --rm \
         -v ${BASEDIR}/target/failsafe-reports:/fitnesse/target/failsafe-reports \
-        -v ${BASEDIR}/target/fitnesse-results/chrome:/fitnesse/target/fitnesse-results \
-        -v ${BASEDIR}/target/fitnesse-results/chrome-rerun:/fitnesse/target/fitnesse-rerun-results \
-        -v ${BASEDIR}/target/selenium-log:/fitnesse/target/selenium-log \
+        -v ${BASEDIR}/target/fitnesse-results/chrome-pdf:/fitnesse/target/fitnesse-results \
+        -v ${BASEDIR}/target/fitnesse-results/chrome-pdf-rerun:/fitnesse/target/fitnesse-rerun-results \
+        -v ${BASEDIR}/target/selenium-log:/fitnesse/target/selenium-log-pdf \
         -v ${BASEDIR}/src/main/wiki:/fitnesse/wiki/FitNesseRoot \
         -e RE_RUN_FAILED=true \
         ${IMAGE} \
--- buildTestWithPdf.sh
+++ buildTestWithPdf.sh
@@ -6,8 +6,8 @@ IMAGE=hsac/fitnesse-fixtures-test-jre8-with-pdf:latest
 docker build --squash -t ${IMAGE} test-with-pdf \
     && docker run --rm  \
         -v ${BASEDIR}/target/failsafe-reports:/fitnesse/target/failsafe-reports \
-        -v ${BASEDIR}/target/fitnesse-results/test:/fitnesse/target/fitnesse-results \
-        -v ${BASEDIR}/target/fitnesse-results/test-rerun:/fitnesse/target/fitnesse-rerun-results \
+        -v ${BASEDIR}/target/fitnesse-results/test-pdf:/fitnesse/target/fitnesse-results \
+        -v ${BASEDIR}/target/fitnesse-results/test-pdf-rerun:/fitnesse/target/fitnesse-rerun-results \
         -v ${BASEDIR}/src/main/wiki:/fitnesse/wiki/FitNesseRoot \
         -e RE_RUN_FAILED=true \
         ${IMAGE} \

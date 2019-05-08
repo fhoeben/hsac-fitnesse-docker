4793d6f33e3eff4164f72ab798b715c3b93d9021
Fried Hoeben <github@hsac.nl>
2019-05-08 21:48:10 +0200

    Ensure we have separate results from runs using version suffix. Fix selenium log copy for chrome-pdf

--- buildChrome.sh
+++ buildChrome.sh
@@ -11,9 +11,9 @@ docker pull selenium/standalone-chrome:${SELENIUM_VERSION}
 docker build --squash --build-arg SELENIUM_VERSION=${SELENIUM_VERSION} -t ${IMAGE} chrome \
     && docker run --rm \
         -v ${BASEDIR}/target/failsafe-reports:/fitnesse/target/failsafe-reports \
-        -v ${BASEDIR}/target/fitnesse-results/chrome:/fitnesse/target/fitnesse-results \
-        -v ${BASEDIR}/target/fitnesse-results/chrome-rerun:/fitnesse/target/fitnesse-rerun-results \
-        -v ${BASEDIR}/target/selenium-log:/fitnesse/target/selenium-log \
+        -v ${BASEDIR}/target/fitnesse-results/chrome${VERSION_SUFFIX}:/fitnesse/target/fitnesse-results \
+        -v ${BASEDIR}/target/fitnesse-results/chrome-rerun${VERSION_SUFFIX}:/fitnesse/target/fitnesse-rerun-results \
+        -v ${BASEDIR}/target/selenium-log${VERSION_SUFFIX}:/fitnesse/target/selenium-log \
         -v ${BASEDIR}/src/main/wiki:/fitnesse/wiki/FitNesseRoot \
         -e RE_RUN_FAILED=true \
         ${IMAGE} \

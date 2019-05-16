c04ee74ef97c92e7d30b741e945adf62634b6b27
Fried Hoeben <github@hsac.nl>
2019-05-16 20:34:07 +0200

    No need to have interactive docker container to create reports

--- combineReports.sh
+++ combineReports.sh
@@ -3,7 +3,7 @@
 BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 IMAGE=hsac/fitnesse-fixtures-test-jre8:latest
 
-docker run --rm -it --entrypoint /fitnesse/htmlReportIndexGenerator.sh \
+docker run --rm --entrypoint /fitnesse/htmlReportIndexGenerator.sh \
     -v ${BASEDIR}/target/fitnesse-results/:/fitnesse/target/fitnesse-results \
      ${IMAGE}
 

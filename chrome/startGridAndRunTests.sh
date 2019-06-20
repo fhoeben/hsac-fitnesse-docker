2ca6708e56bde329792f59c36dac55decef866f9
Fried Hoeben <github@hsac.nl>
2019-06-20 23:06:20 +0200

    Update startup script to wait for selenium grid to be fully started (based on description at https://github.com/SeleniumHQ/docker-selenium/blob/master/README.md#using-a-bash-script-to-wait-for-the-grid).

--- chrome/startGridAndRunTests.sh
+++ chrome/startGridAndRunTests.sh
@@ -1,4 +1,15 @@
 #!/usr/bin/env sh
 
 /opt/bin/entry_point.sh > target/selenium-log/selenium.log &
+
+set -e
+
+while ! curl -sSL "http://localhost:4444/wd/hub/status" 2>&1 \
+        | jq -r '.value.ready' 2>&1 | grep "true" >/dev/null; do
+    echo 'Waiting for the Grid'
+    sleep 1
+done
+
+>&2 echo "Selenium Grid is up - executing tests"
+
 ./runTests.sh $@

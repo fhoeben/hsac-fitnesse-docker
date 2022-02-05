de8b135ab1f93143e24cf9d13f6f6b7004ac9f4b
Fried Hoeben <github@hsac.nl>
2022-02-05 14:24:27 +0100

    Switch to native image graalVM image

--- buildCombine.sh
+++ buildCombine.sh
@@ -6,7 +6,7 @@ BUSYBOX_VERSION=latest
 BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 IMAGE=hsac/fitnesse-fixtures-combine:latest
 
-docker pull ghcr.io/graalvm/graalvm-ce:${GRAALVM_VERSION}
+docker pull ghcr.io/graalvm/native-image:${GRAALVM_VERSION}
 docker pull busybox:${BUSYBOX_VERSION}
 
 docker build --platform linux/amd64 --squash -t ${IMAGE} combine

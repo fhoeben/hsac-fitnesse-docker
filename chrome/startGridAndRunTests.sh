2df193cf0c8ea10d8b1f8233229e6a2c2bbbb873
Fried Hoeben <github@hsac.nl>
2023-11-19 23:27:27 +0100

    add test of combine and artifact of test results

--- chrome/startGridAndRunTests.sh
+++ chrome/startGridAndRunTests.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+# ensure user of container is owner of all mounted volumes
+sudo chown -R 1200:1201 /fitnesse/
+
 /opt/bin/entry_point.sh &
 while [ ! -e /tmp/.X11-unix/X99 ]; do sleep 0.1; done
 

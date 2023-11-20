5e1800d3f6ea66854b84bc065e38c74cb48f7022
Fried Hoeben <github@hsac.nl>
2023-11-20 08:45:03 +0100

    sync with hsac-fitnesse-fixtures

--- chrome/startGridAndRunTests.sh
+++ chrome/startGridAndRunTests.sh
@@ -2,8 +2,13 @@
 
 set -e
 
-# ensure user of container is owner of all mounted volumes
-sudo chown -R 1200:1201 /fitnesse/
+if [ "${CHANGE_FITNESSE_DIRECTOY_OWNER}" = "true" ]; then
+  # ensure user of container is owner of all mounted volumes
+  owner="$(whoami)"
+  user_id="$(id -u "${owner}")"
+  group_id="$(id -g "${owner}")"
+  sudo chown -R "${user_id}:${group_id}" /fitnesse/
+fi
 
 /opt/bin/entry_point.sh &
 while [ ! -e /tmp/.X11-unix/X99 ]; do sleep 0.1; done
